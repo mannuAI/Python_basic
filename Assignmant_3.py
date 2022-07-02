9df696a5470d3de5299d79684cef8f3682965c81
mannuAI <106888705+mannuAI@users.noreply.github.com>
2022-07-02 23:24:46 +0300

    Add files via upload
    Please suggest if any changes requird??

---
+++ Assignmant_3.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Why are functions advantageous to have in your programs?
+# 
+# 
+# Ans:- Functions are use for enhance the readability of a program. Breaking code into smaller functions will make the program organized, easy to understand, and reusable. This avoids repetition of the same code and chances of error.
+
+# # When does the code in a function run: when it is specified or when it is called?
+# 
+# Ans:- The code inside a function is not executed when the function is defined, however function is executed when the function is invoked.It is common to use the term "call a function" instead of "invoke a function".
+# It is also common to say "call upon a function", "start a function", or "execute a function".
+
+# # What statement creates a function?
+# 
+# Ans:- A statement that executes a function. It consists of the name of the function followed by a list of arguments enclosed in parentheses. Using the output from one function call as the input to another.
+
+# # What is the difference between a function and a function call?
+# 
+# Ans:-A function is a piece of code which enhanced the reusability and modularity of your program. It means that piece of code need not be written again.
+# A function call means invoking or calling that function. Unless a function is called there is no use of that function. 
+
+# # What happens to variables in a local scope when the function call returns?
+# 
+# Ans:- When the execution of the function terminates (returns), the local variables are destroyed. Codelens helps you visualize this because the local variables disappear after the function returns.
+
+# # What is the concept of a return value? Is it possible to have a return value in an expression? 
+# 
+# 
+# Ans:- In general, a function takes arguments (if any), performs some operations, and returns a value (or object). The value that a function returns to the caller is generally known as the function's return value.
+
+# # If a function does not have a return statement, what is the return value of a call to that function?
+# 
+# Ans:- If a function doesn't specify a return value, it returns None.
+
+# # How do you make a function variable refer to the global variable?
+# 
+# Ans:- If we want to refer to a global variable in a function, we can use the global keyword to declare which variables are global.
+
+# # What is the data type of None?
+# 
+# Ans:- The None keyword is used to define a null value, or no value at all. None is not the same as 0, False, or an empty string. None is a data type of its own (NoneType) and only None can be None.
+
+# # What does the sentence import areallyourpetsnamederic do?
+# 
+# Ans:- The import statement imports a module named areallyourpetsnamederic.
+
+#  # If you had a bacon() feature in a spam module, what would you call it after importing spam? 
+# 
+# 
+# Ans:-  This function can be called with spam. bacon().
+
+# # What can you do to save a programme from crashing if it encounters an error?
+# 
+# Ans:- If an error occurs in a program, we don’t want the program to unexpectedly crash on the user. Instead, error handling can be used to notify the user of why the error occurred and gracefully exit the process that caused the error.
+
+# # What is the purpose of the try clause? What is the purpose of the except clause?
+# 
+# 
+# Ans:- The try block lets you test a block of code for errors. The except block lets you handle the error.
