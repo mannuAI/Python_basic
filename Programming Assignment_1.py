dff1d72f5588a9038dc7f0aa6ed3642b153757f3
mannuAI <106888705+mannuAI@users.noreply.github.com>
2022-07-03 00:26:18 +0300

    Add files via upload

---
+++ Programming Assignment_1.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Write a Python program to print "Hello Python" ?
+
+# In[1]:
+
+
+print ("Hello Python")
+
+
+# # Write a Python program to do arithmetical operations addition and division.? 
+
+# In[6]:
+
+
+a=12
+b=4
+c=a/b
+print (c)
+
+
+# In[7]:
+
+
+a=12
+b=4
+x=a+b
+print(x)
+
+
+# # Write a Python program to find the area of a triangle?
+
+# In[10]:
+
+
+b=base = 15
+h=hight =34
+y= area_of_triangle= b*h/2
+print (y)
+
+
+# # Write a Python program to swap two variables?
+
+# In[11]:
+
+
+x = 5
+y = 10
+temp = x
+x = y
+y = temp
+
+print('The value of x after swapping: {}'.format(x))
+print('The value of y after swapping: {}'.format(y))
+
+
+# # Write a Python program to generate a random number?
+
+# In[13]:
+
+
+import random
+
+print (random.randint (1,15))
+
+
+# In[ ]:
+
+
+
+
